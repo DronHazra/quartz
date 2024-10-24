0650510fd4f475a79c60f9eea8979365f9628055
Dron Hazra <dronh.to@gmail.com>
2024-10-24 12:25:27 +0100

    changed default date to modified

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     baseUrl: "garden.dronhazra.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
